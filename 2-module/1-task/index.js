71da8f618c700e59cf3b8c9f9fa07dea0dfbf76e
SuperNinjaKook <victorianazarova56@gmail.com>
2023-09-23 20:57:35 +0300

    Закончила выполнение 1-го задания 2-го занятия: Объекты

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,23 @@
-function sumSalary(salaries) {
-  // ваш код...
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+};
+
+function sumSalary(salariesObj) {
+  let sum = 0;
+  for (let key in salariesObj) {
+    // console.log(key);
+    // console.log(salariesObj[key]);
+    if (Number.isFinite(salariesObj[key])) {
+      sum = sum + salariesObj[key];
+    }
+  }
+  console.log(sum);
+  return sum;
 }
+
+sumSalary(salaries);
