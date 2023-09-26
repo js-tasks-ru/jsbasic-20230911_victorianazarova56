330694dbb544e3d546e8ba79382de8271a2198dd
SuperNinjaKook <victorianazarova56@gmail.com>
2023-09-26 23:45:09 +0300

    Закончила выполнение 4-го задания 3-го занятия: Функции и маccивы

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,7 @@
 function showSalary(users, age) {
-  // ваш код...
+  let result = users
+    .filter(user => user.age <= age)
+    .map((user, index, array) => index < array.length - 1 ? `${user.name}, ${user.balance}\n` : `${user.name}, ${user.balance}`)
+    .join('');
+  return result;
 }
