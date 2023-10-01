b021907d3dede2e27ae8ce139504ebe80d62ab6d
SuperNinjaKook <victorianazarova56@gmail.com>
2023-10-01 17:42:29 +0300

    Исправила решение 4-го задания 3-го занятия: Функции и маccивы

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,7 +1,7 @@
 function showSalary(users, age) {
   let result = users
     .filter(user => user.age <= age)
-    .map((user, index, array) => index < array.length - 1 ? `${user.name}, ${user.balance}\n` : `${user.name}, ${user.balance}`)
-    .join('');
+    .map((user) => `${user.name}, ${user.balance}`)
+    .join('\n');
   return result;
 }
